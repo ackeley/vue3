f69c5aec31241dd466d48fe6b6e34a4ede2e6d5f
ackeley <ackeleyp@drconsulta.com>
2021-06-05 07:06:23 -0300

    Diretivas v-show e v-if, normalmente utilizado o v-if pois os elemento quando v-if não e renderizado na tela, diferente do v-show que aplica um display none.

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const app = {
                     product: {
                         title: 'Vue 3',
                         description: 'Curso Vue 3',
-                        image: './assets/images/vue3.jpeg'
+                        image: './assets/images/vue3.jpeg',
+                        stars: '3',
                     },
                     product2: {
                         title: 'Html 5',
                         description: 'Curso Vue 3',
-                        image: './assets/images/html5.png'
+                        image: './assets/images/html5.png',
+                        stars: '0',
                     },
                 }
             },
